3c75487de367f1c26ac09b474e7a45513c5d61ea
raddan104 <skyraddan@outlook.com>
2024-07-17 19:09:56 +0300

    preparation commit

---
+++ src/main/java/org/raddan/newspaper/NewspaperApplication.java
@@ -0,0 +1,13 @@
+package org.raddan.newspaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspaperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewspaperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/raddan/newspaper/NewspaperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.raddan.newspaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspaperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
