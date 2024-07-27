d9e9c076919fb12f0b477c09b229fb358e365506
raddan104 <skyraddan@outlook.com>
2024-07-27 19:10:11 +0300

    Added new endpoints to SecurityFilterChain

--- src/main/java/org/raddan/newspaper/security/SecurityConfiguration.java
+++ src/main/java/org/raddan/newspaper/security/SecurityConfiguration.java
@@ -37,7 +37,6 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                // Своего рода отключение CORS (разрешение запросов со всех доменов)
                 .cors(cors -> cors.configurationSource(request -> {
                     var corsConfiguration = new CorsConfiguration();
                     corsConfiguration.setAllowedOriginPatterns(List.of("*"));
@@ -46,10 +45,8 @@ public class SecurityConfiguration {
                     corsConfiguration.setAllowCredentials(true);
                     return corsConfiguration;
                 }))
-                // Настройка доступа к конечным точкам
                 .authorizeHttpRequests(request -> request
-                        // Можно указать конкретный путь, * - 1 уровень вложенности, ** - любое количество уровней вложенности
-                        .requestMatchers("/auth/**", "/admin/**").permitAll()
+                        .requestMatchers("/auth/**", "/admin/**", "/profile/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/swagger-resources/*", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/endpoint", "/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
