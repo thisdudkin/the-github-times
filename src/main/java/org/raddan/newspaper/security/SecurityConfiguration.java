12f57de126b979a3990f820cef9a8e0ce9247744
raddan104 <skyraddan@outlook.com>
2024-07-27 16:02:25 +0300

    Endpoint for admin-panel

--- src/main/java/org/raddan/newspaper/security/SecurityConfiguration.java
+++ src/main/java/org/raddan/newspaper/security/SecurityConfiguration.java
@@ -49,7 +49,7 @@ public class SecurityConfiguration {
                 // Настройка доступа к конечным точкам
                 .authorizeHttpRequests(request -> request
                         // Можно указать конкретный путь, * - 1 уровень вложенности, ** - любое количество уровней вложенности
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/**", "/admin/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/swagger-resources/*", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/endpoint", "/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
