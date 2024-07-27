a6daff4f4d25806ceaa7fe58d39aee1e05ad2b44
raddan104 <skyraddan@outlook.com>
2024-07-27 17:33:47 +0300

    JavaDoc for AdminPanel

--- src/main/java/org/raddan/newspaper/config/AdminPanel.java
+++ src/main/java/org/raddan/newspaper/config/AdminPanel.java
@@ -16,37 +16,60 @@ public class AdminPanel implements AdminActions {
     @Autowired
     private UserRepository userRepo;
 
-
+    /**
+     * Implementation of method {@code ban} in the interface {@code AdminActions}
+     * @param user - specific user
+     */
     @Override
     public void ban(User user) {
         user.setIsActive(false);
         userRepo.save(user);
     }
 
+    /**
+     * Implementation of method {@code unban} in the interface {@code AdminActions}
+     * @param user - specific user
+     */
     @Override
     public void unban(User user) {
         user.setIsActive(true);
         userRepo.save(user);
     }
 
+    /**
+     * Implementation of method {@code grantAdmin} in the interface {@code AdminActions}
+     * @param user - specific user
+     */
     @Override
     public void grantAdmin(User user) {
         user.setRole(ROLE_ADMIN);
         userRepo.save(user);
     }
 
+    /**
+     * Implementation of method {@code revokeAdmin} in the interface {@code AdminActions}
+     * @param user - specific user
+     */
     @Override
     public void revokeAdmin(User user) {
         user.setRole(ROLE_USER);
         userRepo.save(user);
     }
 
+    /**
+     * Implementation of method {@code grantModerator} in the interface {@code AdminActions}
+     * @param user - specific user
+     */
     @Override
     public void grantModerator(User user) {
         user.setRole(ROLE_MODERATOR);
         userRepo.save(user);
     }
 
+    /**
+     * Implementation of method {@code revokeModerator} in the interface {@code AdminActions}
+     * @param user - specific user
+     */
     @Override
     public void revokeModerator(User user) {
         user.setRole(ROLE_USER);
