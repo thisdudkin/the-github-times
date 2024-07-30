2cd8363eb1df6c9ef97511942950a4ba7d0693f1
earlspilner <alexraddan@gmail.com>
2024-07-30 10:55:39 +0300

    Written generic method for updating entities field

---
+++ src/main/java/org/raddan/newspaper/service/EntityFieldUpdater.java
@@ -0,0 +1,39 @@
+package org.raddan.newspaper.service;
+
+import org.raddan.newspaper.dto.ProfileDTO;
+import org.raddan.newspaper.entity.Profile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author Alexander Dudkin
+ */
+@Component
+public class EntityFieldUpdater {
+
+    private static final Logger log = LoggerFactory.getLogger(EntityFieldUpdater.class);
+
+    public <T, D> T update(T entity, D data) {
+        for (Field field : entity.getClass().getDeclaredFields()) {
+            for (Field dtoField : data.getClass().getDeclaredFields()) {
+                dtoField.setAccessible(true);
+                if (dtoField.getName().equals(field.getName())) {
+                    try {
+                        field.setAccessible(true);
+                        field.set(entity, dtoField.get(data));
+                    } catch (IllegalAccessException e) {
+                        log.error("Error updating profile: {}", e.getMessage());
+                        throw new RuntimeException("Failed to update profile", e);
+                    }
+                }
+            }
+        }
+
+        log.info("User: updated profile successfully!");
+        return entity;
+    }
+
+}
