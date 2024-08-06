9fbd3506dd0c7f17eb8f79c5f2c5b7fa7c928553
earlspilner <alexraddan@gmail.com>
2024-08-06 23:45:48 +0300

    chore: removed unused imports

--- src/main/java/org/raddan/newspaper/exception/GlobalExceptionHandler.java
+++ src/main/java/org/raddan/newspaper/exception/GlobalExceptionHandler.java
@@ -2,14 +2,11 @@ package org.raddan.newspaper.exception;
 
 import org.raddan.newspaper.exception.custom.*;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.lang.reflect.InvocationTargetException;
