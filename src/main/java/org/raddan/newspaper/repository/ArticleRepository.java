91d3c162206f47bcf315679a3b55189e7351b9c6
earlspilner <alexraddan@gmail.com>
2024-08-12 21:09:37 +0300

    refactor: removed unused ArticleRepository's method

--- src/main/java/org/raddan/newspaper/repository/ArticleRepository.java
+++ src/main/java/org/raddan/newspaper/repository/ArticleRepository.java
@@ -2,15 +2,8 @@ package org.raddan.newspaper.repository;
 
 import org.raddan.newspaper.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.Optional;
 
 /**
  * @author Alexander Dudkin
  */
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-    @Query("SELECT a FROM Article a WHERE a.title = :p_title")
-    Optional<Article> findByTitle(@Param("p_title") String title);
-}
+public interface ArticleRepository extends JpaRepository<Article, Long> { }
