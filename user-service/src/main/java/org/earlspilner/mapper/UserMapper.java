a5ab294bd227e2622b906bd338d1e550fc2a7c24
Alexander <alexraddan@gmail.com>
2024-08-19 19:26:52 +0300

    fix: few changes to make build complete

--- user-service/src/main/java/org/earlspilner/mapper/UserMapper.java
+++ user-service/src/main/java/org/earlspilner/mapper/UserMapper.java
@@ -2,16 +2,12 @@ package org.earlspilner.mapper;
 
 import org.earlspilner.dto.UserDto;
 import org.earlspilner.models.User;
-import org.earlspilner.models.UserRole;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 /**
  * @author Alexander Dudkin
  */
 @Mapper(componentModel = "spring", uses = UserRoleMapper.class)
 public interface UserMapper {
-    @Mapping(source = "role", target = "role")
     User toUser(UserDto dto);
 }
