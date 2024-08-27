e67773ef58a73365bc7e90705b37a147d162ed9a
Alexander Dudkin <alexraddan@gmail.com>
2024-08-28 01:04:02 +0300

    first commit

---
+++ the-github-times-users-service/src/main/java/org/earlspilner/thegithubtimesusersservice/TheGithubTimesUsersServiceApplication.java
@@ -0,0 +1,13 @@
+package org.earlspilner.thegithubtimesusersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGithubTimesUsersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheGithubTimesUsersServiceApplication.class, args);
+    }
+
+}
