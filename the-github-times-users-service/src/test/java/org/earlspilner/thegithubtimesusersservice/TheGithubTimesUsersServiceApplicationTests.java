29070f9db363187e221f0eee73809f35c42466eb
Alexander Dudkin <alexraddan@gmail.com>
2024-08-28 01:04:02 +0300

    first commit

---
+++ the-github-times-users-service/src/test/java/org/earlspilner/thegithubtimesusersservice/TheGithubTimesUsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.earlspilner.thegithubtimesusersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGithubTimesUsersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
