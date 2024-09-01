7b3762d5dcd3339624042d7072ca7b3d2501bb9a
Alexander Dudkin <alexraddan@gmail.com>
2024-09-01 21:33:34 +0300

    test: add UserServiceApplicationTests for main class of users service

---
+++ users-service/src/test/java/dev/earlspilner/users/UserServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.earlspilner.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Alexander Dudkin
+ */
+@SpringBootTest
+public class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
