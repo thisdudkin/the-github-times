13416e27fea0c50127abdecff9477a3407b88312
Alexander Dudkin <alexraddan@gmail.com>
2024-09-02 23:06:25 +0300

    refactor: a couple of changes

--- authentication-server/src/main/java/org/earlspilner/auth/advice/ExceptionControllerAdvice.java
+++ authentication-server/src/main/java/org/earlspilner/auth/advice/ExceptionControllerAdvice.java
@@ -1,5 +1,6 @@
 package org.earlspilner.auth.advice;
 
+import feign.FeignException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,8 @@ import org.springframework.web.context.request.WebRequest;
 
 import java.net.URI;
 import java.time.OffsetDateTime;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static java.time.OffsetDateTime.now;
 import static org.springframework.http.HttpStatus.*;
@@ -37,9 +40,20 @@ public class ExceptionControllerAdvice {
     @ExceptionHandler(UsernameNotFoundException.class)
     public ResponseEntity<ProblemDetail> handleUsernameNotFoundException(UsernameNotFoundException ex, WebRequest request) {
         return createProblemDetail(
-                "Bad credentials",
+                "User not found",
                 NOT_FOUND.value(),
                 ex.getLocalizedMessage(),
+                request,
+                now()
+        );
+    }
+
+    @ExceptionHandler(FeignException.FeignClientException.class)
+    public ResponseEntity<ProblemDetail> handleFeignClientException(FeignException ex, WebRequest request) {
+        return createProblemDetail(
+                "Bad credentials",
+                NOT_FOUND.value(),
+                extractRelevantFeignMessage(ex.getMessage()),
                 request,
                 now()
         );
@@ -48,7 +62,7 @@ public class ExceptionControllerAdvice {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ProblemDetail> handleRuntimeException(RuntimeException ex, WebRequest request) {
         return createProblemDetail(
-                "Something went wrong..",
+                "Unexpected error",
                 INTERNAL_SERVER_ERROR.value(),
                 ex.getLocalizedMessage(),
                 request,
@@ -64,6 +78,15 @@ public class ExceptionControllerAdvice {
         problemDetail.setProperty("timestamp", timestamp);
 
         return new ResponseEntity<>(problemDetail, HttpStatus.valueOf(status));
+    }
+
+    private String extractRelevantFeignMessage(String message) {
+        Pattern pattern = Pattern.compile("\\[(\\d{3})] during \\[(GET|POST|PUT|DELETE|PATCH)] to \\[.*?\\]");
+        Matcher matcher = pattern.matcher(message);
+        if (matcher.find()) {
+            return matcher.group(0);
+        }
+        return "Feign client error occurred.";
     }
 
 }
