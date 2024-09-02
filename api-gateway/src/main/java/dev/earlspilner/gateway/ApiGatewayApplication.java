93ce13b1e6049a0fd65852a6bcb47d8ad1c14904
Alexander Dudkin <alexraddan@gmail.com>
2024-09-02 16:42:49 +0300

    feat: add Spring Cloud Gateway to manage & centralize microservices routes

---
+++ api-gateway/src/main/java/dev/earlspilner/gateway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package dev.earlspilner.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Alexander Dudkin
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
