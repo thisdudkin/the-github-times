a41c4cc2b340fe487a947249fe1f3b04bdf981b6
Alexander Dudkin <alexraddan@gmail.com>
2024-09-04 11:18:46 +0300

    feat: add routing for all microservices in java config

--- api-gateway/src/main/java/dev/earlspilner/gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/dev/earlspilner/gateway/config/GatewayConfig.java
@@ -15,6 +15,9 @@ import org.springframework.context.annotation.Configuration;
 public class GatewayConfig {
 
     static final String USERS_SERVICE = "http://localhost:9090";
+    static final String AUTH_SERVER = "http://localhost:6969";
+    static final String PROFILES_SERVICE = "http://localhost:9091";
+    static final String ARTICLES_SERVICE = "http://localhost:9093";
 
     private final AuthenticationFilter filter;
 
@@ -29,6 +32,15 @@ public class GatewayConfig {
                 .route("users-service", r -> r.path("/api/users/**")
                         .filters(f -> f.filter(filter))
                         .uri(USERS_SERVICE))
+                .route("auth-server", r -> r.path("/api/auth/**")
+                        .filters(f -> f.filter(filter))
+                        .uri(AUTH_SERVER))
+                .route("profiles-service", r -> r.path("/api/profiles/**")
+                        .filters(f -> f.filter(filter))
+                        .uri(PROFILES_SERVICE))
+                .route("articles-service", r -> r.path("/api/articles/**")
+                        .filters(f -> f.filter(filter))
+                        .uri(ARTICLES_SERVICE))
                 .build();
     }
 
