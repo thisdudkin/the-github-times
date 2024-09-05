09bb7531ede528331dbd3684c76e2bc30be0717f
Alexander Dudkin <alexraddan@gmail.com>
2024-09-05 09:39:41 +0300

    refactor: add PreAuthorize on deleteUser method

--- users-service/src/main/java/dev/earlspilner/users/controller/UserController.java
+++ users-service/src/main/java/dev/earlspilner/users/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -39,6 +40,7 @@ public class UserController {
         return new ResponseEntity<>(userService.updateUser(username, userDto), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{username}")
     public ResponseEntity<Void> deleteUser(@PathVariable String username) {
         userService.deleteUser(username);
